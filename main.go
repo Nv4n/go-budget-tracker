48bd4a4a46d1fac06db5a7c0e0c4b3559d784fe1
Sybatron <vipsister2014@gmail.com>
2023-11-14 14:01:17 +0200

    Added Hot Loading

--- main.go
+++ main.go
@@ -13,12 +13,14 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl, _ := template.New("").ParseFiles("templates/index.html")
 		err := tmpl.ExecuteTemplate(w, "Base", nil)
 		if err != nil {
 			return
 		}
+
 	})
 	log.Fatal(http.ListenAndServe("localhost:3000", r))
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,15 @@
 module.exports = {
 	darkMode: ["class"],
-	content: ["./templates/**/*.html", "node_modules/preline/dist/*.js"],
-	theme: {
-		container: {
-			center: true,
-			padding: "2rem",
-			screens: {
-				"2xl": "1400px",
-			},
-		},
+	content: [
+		"./templates/**/*{.html,.templ}",
+		"node_modules/preline/dist/*.js",
+	],
+	theme:{
+		extend:{}
 	},
-	plugins: [require("tailwindcss-animate"), require("preline/plugin")],
+	plugins: [
+		require("tailwindcss-animate"),
+		require("@tailwindcss/forms"),
+		require("preline/plugin"),
+	],
 };
